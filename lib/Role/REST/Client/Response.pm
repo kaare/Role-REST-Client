863f8cdf90c41cad05041f6514c9c03eb43ff898
Mark Stosberg <mark@summersault.com>
2012-11-16 09:17:59 -0500

    A number of doc improvements, fixes #15 and addresses #16
        - Suitable alternatives for HTTP::Tiny have been further clarified
    
        - When "default" or "builder" have been used, it's important to
          communicat which has been done, so people can override it. If you
          use a different option then the role you are consuming, Moose
          throws an exception about a conflict between 'default' and
          'builder'.
    
        - Methods available for managing headers have more complete
          documentation
    
        - Synopsis and other touch-ups for Role::REST::Client docs
    
        - Some minor whitespace clean-ups as well. (Removing a bit of
          end-of-line whitespace)

--- lib/Role/REST/Client/Response.pm
+++ lib/Role/REST/Client/Response.pm
@@ -33,19 +33,29 @@ __END__
 
 Role::REST::Client::Response - Response class for REST
 
-=head1 METHODS
+=head1 SYNOPSIS
+
+    my $res = Role::REST::Client::Response->new(
+        code          => '200',
+        response      => HTTP::Response->new(...),
+        error         => 0,
+        data_callback => sub { sub { ... } },
+    );
+
+=head1 ATTRIBUTES
 
 =head2 code
 
-Returns the http status code of the request
+HTTP status code of the request
 
 =head2 response
 
-Returns the a HTTP::Response object. Use this if you need more information than status and content.
+L<HTTP::Response> object. Use this if you need more information than status and content.
 
 =head2 error
 
-Returns the returned reason from HTTP::Tiny where the status is 500 or higher. 
+The returned reason from L<HTTP::Tiny> where the status is 500 or higher. More detail may be provided 
+by calling C<< $res->response->content >>.
 
 =head2 failed
 
@@ -53,21 +63,21 @@ True if the request didn't succeed.
 
 =head2 data
 
-Returns the deserialized data. Returns an empty hashref if the response was unsuccessful
+The deserialized data. Returns an empty hashref if the response was unsuccessful.
 
 =head1 AUTHOR
 
 Kaare Rasmussen, <kaare at cpan dot com>
 
-=head1 BUGS 
+=head1 BUGS
 
 Please report any bugs or feature requests to bug-role-rest-client at rt.cpan.org, or through the
 web interface at http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Role-REST-Client.
 
-=head1 COPYRIGHT & LICENSE 
+=head1 COPYRIGHT & LICENSE
 
 Copyright 2012 Kaare Rasmussen, all rights reserved.
 
-This library is free software; you can redistribute it and/or modify it under the same terms as 
-Perl itself, either Perl version 5.8.8 or, at your option, any later version of Perl 5 you may 
+This library is free software; you can redistribute it and/or modify it under the same terms as
+Perl itself, either Perl version 5.8.8 or, at your option, any later version of Perl 5 you may
 have available.
